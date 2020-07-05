cedf346a165991a6b1132369419c5004844d151a
ChanX21 <47290661+ChanX21@users.noreply.github.com>
2020-07-05 18:44:35 +0530

    Update run_analysis.r
    The Final Code For Getting Clean Data Output

--- run_analysis.r
+++ run_analysis.r
@@ -16,28 +16,13 @@ features <-read.table("C:/Users/SUN/Downloads/getdata_projectfiles_UCI HAR Datas
 Xtotal <- rbind(Xtrain, Xtest)   #Merging Row wise  Xtrain and Xtest
 Ytotal <- rbind(Ytrain, Ytest)   #Merging Row wise  Ytrain and Ytest
 SubTotal <- rbind(SubTrain, SubTest) #Merging Row wise  SubTrain and SubTest
-
-# Below Steps are for selecting values with mean and std using grep function to find text matches 
-
-FilteredVars <- features[grep("mean\\(\\)|std\\(\\)",features[,2]),]
-
-FilteredVars <- FilteredVars[c(1:50),]
-
-names(Xtotal) <- FilteredVars[,2]
-
-
-FilteredVars <- features[grep("mean\\(\\)|std\\(\\)",features[,2]),]
-
-FilteredVars <- FilteredVars[c(1:50),]
-
-names(Xtotal) <- FilteredVars[,2]
-NewTidyAverage <- NULL
-
-for ( i in 1 : 50 ){
-  NewTidyAverage <- c(NewTidyAverage,mean(Xtotal[,i]))
-  
-  
-}
-names(NewTidyAverage) <- FilteredVars[,2]
-
-write.table(NewTidyAverage,"C:/Users/SUN/Downloads/getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/NewTidyOutput.txt",col.names = T ,row.names = T)
+names(Xtotal) <- features[,2]
+FVIndices <- grep("mean\\(\\)|std\\(\\)",features[,2],value = F)
+Xtotal <- Xtotal[,FVIndices]
+names(SubTotal) <- "Subject"
+names(Ytotal) <- "Activity"
+Ytotal$ActivityLabel <- factor(Ytotal$Activity , labels = as.character(activityLabels[,2]))
+subYTotAct <- Ytotal$ActivityLabel
+FinalSet <- cbind(Xtotal,SubTotal,subYTotAct)
+FinalCleanSum <- FinalSet %>% group_by(subYTotAct,Subject) %>% summarize_each(funs(mean))
+write.table(FinalCleanSum,"C:/Users/SUN/Downloads/getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/NewTidyOutput.txt",col.names = T ,row.names = T)
